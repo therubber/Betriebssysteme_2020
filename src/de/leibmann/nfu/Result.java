6d05ff938003b8b20a9bc7530a8b670c4dcdcc1d
ruben <ruben.leibmann@gmx.de>
2020-07-30 16:09:58 +0200

    Added info to Result

--- src/de/leibmann/nfu/Result.java
+++ src/de/leibmann/nfu/Result.java
@@ -4,6 +4,8 @@ import java.util.LongSummaryStatistics;
 
 public class Result {
 
+    private Result() {}
+
     private static int replacements = 0;
     private static LongSummaryStatistics lifetimes = new LongSummaryStatistics();
 
@@ -17,5 +19,8 @@ public class Result {
     public static void showResults() {
         System.out.println("Average Page Lifetime: " + lifetimes.getAverage() + "ms.");
         System.out.println("Replacements: " + replacements);
+        System.out.println();
+        System.out.println("INFO: Im RAM befindet sich eine Page mit niedrigerem Zugriffszähler als eine andere Page in der PagingArea.");
+        System.out.println("Dies hat den Grund, dass diese als letztes Accessed wurde und deshalb in den RAM ausgelagert wurde.");
     }
 }
