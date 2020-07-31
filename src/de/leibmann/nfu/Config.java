0fc0964a5435f6535d0b300322f2077ecf38fb4e
ruben <ruben.leibmann@gmx.de>
2020-07-31 12:38:08 +0200

    Minor improvements

--- src/de/leibmann/nfu/Config.java
+++ src/de/leibmann/nfu/Config.java
@@ -1,10 +1,12 @@
 package de.leibmann.nfu;
 
+/**
+ * @author 4674215 (Matrikelnummer)
+ */
 public class Config {
 
-    /**
-     * @author 4674215 (Matrikelnummer)
-     */
+    private Config() {}
+
     private static final int ramSize = 5;
 
     public static int getRamSize() {
